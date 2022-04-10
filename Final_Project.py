dadef2671b42816864a6c81faebbfbb2c941cfde
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2022-04-10 11:56:03 -0400

    Doing foursquare call to get all schools on all neighborhoods
    Co-Authored-By: Foram <24356597+foramgandhi12@users.noreply.github.com>

--- Final_Project.py
+++ Final_Project.py
@@ -2,22 +2,31 @@ import requests
 import folium
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 load_dotenv()
 
-latitude = 43.6532
-longitude = -79.3832
-
-m = folium.Map(location=[latitude, longitude], zoom_start=12)
-m.save("map.html")
-
-url = "https://api.foursquare.com/v3/places/search?ll=43.6532%2C-79.3832"
-
+# m = folium.Map(location=[latitude, longitude], zoom_start=12)
+# m.save("map.html")
 headers = {
     "Accept": "application/json",
     "Authorization": os.getenv("AUTHORIZATION")
 }
 
-response = requests.request("GET", url, headers=headers)
+dataset = pd.read_csv('Age_Vs_Crime.csv')
+neighborhoods = dataset.columns[:]
+for n in neighborhoods:
+    print(n)
+    url = "https://api.foursquare.com/v3/places/search?query=primary_school%2C%20elementary_school%2C%20secondary_school&near={}%2C%20Toronto".format(n)
+    response = requests.request("GET", url, headers=headers).json()
+    print(response["results"])
 
-print(response.text)
+
+
+
+
+# url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show"
+#
+# params = { "id": "6e19a90f-971c-46b3-852c-0c48c436d1fc" }
+# package = requests.get(url, params=params).json()
+# print(package["result"])
